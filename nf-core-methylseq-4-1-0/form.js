73d4f6bd9e9c3ddde613a35b933a0af27c13ffd3
Yucheng Zhang <Yucheng.Zhang@tufts.edu>
2025-09-23 19:47:42 -0400

    update nf-core pipelines

---
+++ nf-core-methylseq-4-1-0/form.js
@@ -0,0 +1,102 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll("label").forEach((label) => {
+    if (label.textContent.trim() === "TOWER_ACCESS_TOKEN (Optional)") {
+      label.style.color = "green";
+      label.style.fontWeight = "bold";
+    }
+  });
+});
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll("label").forEach((label) => {
+    if (label.textContent.trim() === "Resume") {
+      label.style.color = "green";
+      label.style.fontWeight = "bold";
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll("label").forEach((label) => {
+    if (label.textContent.trim() === "Input/output options") {
+      label.style.color = "green";
+      label.style.fontWeight = "bold";
+    }
+  });
+
+  document.querySelectorAll("label").forEach((label) => {
+    if (label.textContent.trim() === "Save intermediate files") {
+      label.style.color = "green";
+      label.style.fontWeight = "bold";
+    }
+  });
+
+  document.querySelectorAll("label").forEach((label) => {
+    if (label.textContent.trim() === "Reference genome options") {
+      label.style.color = "green";
+      label.style.fontWeight = "bold";
+    }
+  });
+
+  document.querySelectorAll("label").forEach((label) => {
+    if (label.textContent.trim() === "Alignment options") {
+      label.style.color = "green";
+      label.style.fontWeight = "bold";
+    }
+  });
+
+  document.querySelectorAll("label").forEach((label) => {
+    if (label.textContent.trim() === "Special library types") {
+      label.style.color = "green";
+      label.style.fontWeight = "bold";
+    }
+  });
+
+  document.querySelectorAll("label").forEach((label) => {
+    if (label.textContent.trim() === "Adapter Trimming") {
+      label.style.color = "green";
+      label.style.fontWeight = "bold";
+    }
+  });
+
+  document.querySelectorAll("label").forEach((label) => {
+    if (label.textContent.trim() === "Bismark options") {
+      label.style.color = "green";
+      label.style.fontWeight = "bold";
+    }
+  });
+
+  document.querySelectorAll("label").forEach((label) => {
+    if (label.textContent.trim() === "bwa-meth options") {
+      label.style.color = "green";
+      label.style.fontWeight = "bold";
+    }
+  });
+
+  document.querySelectorAll("label").forEach((label) => {
+    if (label.textContent.trim() === "Qualimap Options") {
+      label.style.color = "green";
+      label.style.fontWeight = "bold";
+    }
+  });
+
+  document.querySelectorAll("label").forEach((label) => {
+    if (label.textContent.trim() === "Targeted Sequencing Analysis Options") {
+      label.style.color = "green";
+      label.style.fontWeight = "bold";
+    }
+  });
+
+  document.querySelectorAll("label").forEach((label) => {
+    if (label.textContent.trim() === "Skip pipeline steps") {
+      label.style.color = "green";
+      label.style.fontWeight = "bold";
+    }
+  });
+
+  document.querySelectorAll("label").forEach((label) => {
+    if (label.textContent.trim() === "Run pipeline steps") {
+      label.style.color = "green";
+      label.style.fontWeight = "bold";
+    }
+  });
+});

ff2c435b420796d825595780eeef2ff9466175a4
Yucheng Zhang <Yucheng.Zhang@tufts.edu>
2025-10-01 16:44:10 -0400

    change cpu_partition to partion in nfcore, and add singularity bind in rstudio

---
+++ nf-core-scrnaseq-4-0-0/form.js
@@ -0,0 +1,88 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll("label").forEach((label) => {
+    if (label.textContent.trim() === "TOWER_ACCESS_TOKEN (Optional)") {
+      label.style.color = "green";
+      label.style.fontWeight = "bold";
+    }
+  });
+});
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll("label").forEach((label) => {
+    if (label.textContent.trim() === "Resume") {
+      label.style.color = "green";
+      label.style.fontWeight = "bold";
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll("label").forEach((label) => {
+    if (label.textContent.trim() === "Input/output options") {
+      label.style.color = "green";
+      label.style.fontWeight = "bold";
+    }
+  });
+
+  document.querySelectorAll("label").forEach((label) => {
+    if (label.textContent.trim() === "Mandatory arguments") {
+      label.style.color = "green";
+      label.style.fontWeight = "bold";
+    }
+  });
+
+  document.querySelectorAll("label").forEach((label) => {
+    if (label.textContent.trim() === "Skip Tools") {
+      label.style.color = "green";
+      label.style.fontWeight = "bold";
+    }
+  });
+
+  document.querySelectorAll("label").forEach((label) => {
+    if (label.textContent.trim() === "Reference genome options") {
+      label.style.color = "green";
+      label.style.fontWeight = "bold";
+    }
+  });
+
+  document.querySelectorAll("label").forEach((label) => {
+    if (label.textContent.trim() === "Simpleaf Options") {
+      label.style.color = "green";
+      label.style.fontWeight = "bold";
+    }
+  });
+
+  document.querySelectorAll("label").forEach((label) => {
+    if (label.textContent.trim() === "STARSolo Options") {
+      label.style.color = "green";
+      label.style.fontWeight = "bold";
+    }
+  });
+
+  document.querySelectorAll("label").forEach((label) => {
+    if (label.textContent.trim() === "Kallisto/BUS Options") {
+      label.style.color = "green";
+      label.style.fontWeight = "bold";
+    }
+  });
+
+  document.querySelectorAll("label").forEach((label) => {
+    if (label.textContent.trim() === "Cellranger Options") {
+      label.style.color = "green";
+      label.style.fontWeight = "bold";
+    }
+  });
+
+  document.querySelectorAll("label").forEach((label) => {
+    if (label.textContent.trim() === "Cellranger ARC Options") {
+      label.style.color = "green";
+      label.style.fontWeight = "bold";
+    }
+  });
+
+  document.querySelectorAll("label").forEach((label) => {
+    if (label.textContent.trim() === "Cellranger Multi options") {
+      label.style.color = "green";
+      label.style.fontWeight = "bold";
+    }
+  });
+});
